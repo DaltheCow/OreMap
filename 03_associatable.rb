4555b53ce0e3f233ee7662eefb15c885240ffa1a
Ben Blue <dalcowboiz@gmail.com>
2017-11-07 17:15:24 -0500

    finished OreMap

--- 03_associatable.rb
+++ 03_associatable.rb
@@ -1,61 +1,81 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
 
+# Phase IIIa
 class AssocOptions
-  attr_accessor(
-    :foreign_key,
-    :class_name,
-    :primary_key
-  )
+  attr_accessor :foreign_key, :class_name, :primary_key
 
   def model_class
-    self.class_name.constantize
+    @class_name.constantize
   end
 
   def table_name
-    return "humans" if self.class_name.downcase === "human"
-    self.class_name.downcase.pluralize
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    self.foreign_key = options[:foreign_key] || "#{name.to_s}_id".to_sym
-    self.primary_key = options[:primary_key] || :id
-    self.class_name = options[:class_name] || name.to_s.capitalize
+    defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    self.foreign_key = options[:foreign_key] || "#{self_class_name.downcase}_id".to_sym
-    self.primary_key = options[:primary_key] || :id
-    self.class_name = options[:class_name] || name.to_s.singularize.capitalize
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 module Associatable
-
+  # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
 
     define_method(name) do
-      key = send(options.foreign_key)
-      options.model_class.where(options.primary_key => key)[0]
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.foreign_key)
+      options
+        .model_class
+        .where(options.primary_key => key_val)
+        .first
     end
   end
 
   def has_many(name, options = {})
-    options =  HasManyOptions.new(name, self.name, options)
+    self.assoc_options[name] =
+      HasManyOptions.new(name, self.name, options)
 
     define_method(name) do
-      key = send(options.primary_key)
-      options.model_class.where(options.foreign_key => key)
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.primary_key)
+      options
+        .model_class
+        .where(options.foreign_key => key_val)
     end
   end
 
   def assoc_options
-
+    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
