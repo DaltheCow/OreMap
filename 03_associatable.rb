0c9530e7f98c7cdd4770af7e9d40ea6257af2df5
Ben Blue <dalcowboiz@gmail.com>
2017-11-07 11:19:43 -0500

    add setting belongs to and has many (including defaults)

--- 03_associatable.rb
+++ 03_associatable.rb
@@ -1,7 +1,7 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
 
-# Phase IIIa
+
 class AssocOptions
   attr_accessor(
     :foreign_key,
@@ -10,41 +10,45 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+
   end
 
   def table_name
-    # ...
+
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    self.foreign_key = options[:foreign_key] || "#{name.to_s}_id".to_sym
+    self.primary_key = options[:primary_key] || :id
+    self.class_name = options[:class_name] || name.to_s.capitalize
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    self.foreign_key = options[:foreign_key] || "#{self_class_name.downcase}_id".to_sym
+    self.primary_key = options[:primary_key] || :id
+    self.class_name = options[:class_name] || name.to_s.singularize.capitalize
   end
 end
 
 module Associatable
-  # Phase IIIb
+
   def belongs_to(name, options = {})
-    # ...
+
   end
 
   def has_many(name, options = {})
-    # ...
+
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+
 end
